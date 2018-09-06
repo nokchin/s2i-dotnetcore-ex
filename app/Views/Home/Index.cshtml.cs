73f27d64f62e08c6d639c604aedcf7d77d3c1cbc
nokchin <nokchin@hotmail.com>
2018-09-06 21:19:22 +0800

    Your commit message

--- app/Views/Home/Index.cshtml.cs
+++ app/Views/Home/Index.cshtml.cs
@@ -3,9 +3,10 @@ using System;
 
 namespace RazorPagesIntro.Pages {
   public class IndexModel2 : PageModel  {
-    public string Message { get; private set; } = "PageModel in C#";
+    public string Message = "PageModel in C#";
+/*  public string Message { get; private set; } = "PageModel in C#";
 
-    public void OnGet() {   Message += $" Server time is { DateTime.Now }";   }
+    public void OnGet() {   Message += $" Server time is { DateTime.Now }";   }    */
 
   }
 }
