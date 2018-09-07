9b5cec10fd25b90b8ec2c3f381dc31b466f84a74
nokchin <nokchin@hotmail.com>
2018-09-07 19:47:59 +0800

    Your commit message

--- app/Views/Home/Index.cshtml.cs
+++ app/Views/Home/Index.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LearnRazorPages.Pages {
   public class IndexModel : PageModel {
-    public string Message { get; set; } = "PageModel in C#";
+//  public string Message { get; set; } = "PageModel in C#";
 //  public string Message = "Original";
 //  public void OnGet() {Message="New Message!";}
   }
