77300336f9543fd7ff249f68f87218eb72338838
nokchin <nokchin@hotmail.com>
2018-09-09 19:58:52 +0800

    Your commit message

---
+++ app/Models/IndexModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LearnRazorPages.Pages {
+  public class IndexModel {
+//  public string Message { get; set; } = "PageModel in C#";
+    public string Message = "Original";
+//  public void OnGet() {Message="New Message!";}
+  }
+}
+
+
+
+
+
+
+
+/*  originals ....
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LearnRazorPages.Pages {
+  public class IndexModel : PageModel {
+//  public string Message { get; set; } = "PageModel in C#";
+    public string Message = "Original";
+//  public void OnGet() {Message="New Message!";}
+  }
+}
+*/
