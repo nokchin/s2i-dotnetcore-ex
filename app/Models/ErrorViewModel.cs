2cf8095cdc7314f4358010cba1df32d010e9f24f
nokchin <nokchin@hotmail.com>
2018-09-10 18:13:19 +0800

    Your commit message

--- app/Models/ErrorViewModel.cs
+++ app/Models/ErrorViewModel.cs
@@ -5,7 +5,8 @@ namespace app.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Message = "Hello there!";
     }
 }
