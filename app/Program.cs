93949fe64b0f04e4eafefb8c212e686c4521c7be
nokchin <nokchin@hotmail.com>
2018-10-16 18:01:34 +0800

    Your commit message

--- app/Program.cs
+++ app/Program.cs
@@ -27,20 +27,23 @@ namespace app
 }
 
 
-// ** https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/introduction-to-signalr
-          ->  https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-2.1&tabs=visual-studio
-          ->  https://docs.microsoft.com/en-us/aspnet/core/signalr/index?view=aspnetcore-2.1
-//  https://stackoverflow.com/questions/49816245/asp-net-core-2-1-server-timeout-while-debugging  <-- VERY GOOD!
+/* 
+[*] https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/introduction-to-signalr
+            ->  https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-2.1&tabs=visual-studio
+            ->  https://docs.microsoft.com/en-us/aspnet/core/signalr/index?view=aspnetcore-2.1
+-   https://stackoverflow.com/questions/49816245/asp-net-core-2-1-server-timeout-while-debugging  <-- VERY GOOD!
     https://github.com/aspnet/Docs/issues/6885
-//  https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-nunit , https://docs.microsoft.com/en-us/dotnet/core/
-//  https://stackoverflow.com/questions/34300204/why-doesnt-nunit-timeout-attribute-work
-//  https://jeremylindsayni.wordpress.com/2016/05/28/how-to-set-a-maximum-time-to-allow-a-c-function-to-run-for/
-//  https://stackoverflow.com/questions/37474309/timeouts-with-long-running-asp-net-mvc-core-controller-httppost-method
-//  https://stackoverflow.com/questions/38698350/increase-upload-file-size-in-asp-net-core   [RequestSizeLimit(100_000_000)]  [DisableRequestSizeLimit]
-//  http://www.dotnet-stuff.com/tutorials/aspnet-mvc/understanding-asp-net-mvc-filters-and-attributes     <---
-//  https://www.itprotoday.com/web-development/attributes-controller
-//  https://www.codeproject.com/Tips/1032266/MVC-Attributes
-//  https://blogs.msdn.microsoft.com/aspnetue/2010/02/24/attributes-and-asp-net-mvc/
+-   https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-nunit , https://docs.microsoft.com/en-us/dotnet/core/
+-   https://stackoverflow.com/questions/34300204/why-doesnt-nunit-timeout-attribute-work
+-   https://jeremylindsayni.wordpress.com/2016/05/28/how-to-set-a-maximum-time-to-allow-a-c-function-to-run-for/
+-   https://stackoverflow.com/questions/37474309/timeouts-with-long-running-asp-net-mvc-core-controller-httppost-method
+-   https://stackoverflow.com/questions/38698350/increase-upload-file-size-in-asp-net-core   [RequestSizeLimit(100_000_000)]  [DisableRequestSizeLimit]
+-   http://www.dotnet-stuff.com/tutorials/aspnet-mvc/understanding-asp-net-mvc-filters-and-attributes     <---
+-   https://www.itprotoday.com/web-development/attributes-controller
+-   https://www.codeproject.com/Tips/1032266/MVC-Attributes
+-   https://blogs.msdn.microsoft.com/aspnetue/2010/02/24/attributes-and-asp-net-mvc/
+*/
+
 
 /*  https://msdn.microsoft.com/en-us/library/ms243175.aspx
 Add the Timeout attribute to each test. The parameter is in milliseconds. For example:
@@ -51,6 +54,7 @@ public void MyTestMethod()
 }
 
 */
+
 
 //  https://wcfbiztalk.wordpress.com/2017/12/26/increase-timeout-for-asp-net-core-applications/   <- Good ! !
 //  https://forums.asp.net/t/2135665.aspx?add+requestTimeout+in+web+config
